bd92d4adcacc45da4cb87b26fef2653f94d6f5a1
sananyapoom <sananyapoom2547@gmail.com>
2025-12-14 17:04:14 +0700

    Refactor to MVC: Added DrinkController and routes

--- sever/src/controllers/DrinkController.js
+++ sever/src/controllers/DrinkController.js
@@ -1,23 +1,17 @@
-// controllers/DrinkController.js
-
-// สมมติข้อมูล (ถ้าคุณใช้ DB ให้เปลี่ยนตรงนี้เป็นเรียก DB)
 const drinks = [
   { id: 1, name: "Espresso", price: 40 },
   { id: 2, name: "Latte", price: 50 },
 ];
 
 module.exports = {
-  // 1. Get All (index)
   index: (req, res) => {
     res.json(drinks);
   },
 
-  // 2. Get One (show) -> ที่เราเพิ่งแก้ปัญหาไป
   show: (req, res) => {
     console.log("มาที่ show แล้วนะ ID คือ " + req.params.id);
     const id = req.params.id;
     res.send("Get One ID: " + id);
-    // const id = parseInt(req.params.id);
     const drink = drinks.find((d) => d.id === id);
     if (drink) {
       res.json(drink);
@@ -26,21 +20,15 @@ module.exports = {
     }
   },
 
-  // 3. Create (create)
   create: (req, res) => {
-    // logic การสร้าง...
     res.send("Create drink logic here");
   },
 
-  // 4. Update (update)
   update: (req, res) => {
-    // logic การแก้ไข...
     res.send("Update drink id: " + req.params.id);
   },
 
-  // 5. Delete (delete)
   delete: (req, res) => {
-    // logic การลบ...
     res.send("Delete drink id: " + req.params.id);
   },
 };
--- sever/src/routes.js
+++ sever/src/routes.js
@@ -1,11 +1,9 @@
-// routes.js
 const DrinkController = require('./controllers/DrinkController');
 
 module.exports = (app) => {
-  // เชื่อม URL เข้ากับฟังก์ชันใน Controller
-  app.get("/drinks", DrinkController.index); // Get All
-  app.get('/drink/:id', DrinkController.show); // Get One
-  app.post("/drink", DrinkController.create); // Create
-  app.put("/drink/:id", DrinkController.update); // Update
-  app.delete("/drink/:id", DrinkController.delete); // Delete
+  app.get("/drinks", DrinkController.index); 
+  app.get('/drink/:id', DrinkController.show); 
+  app.post("/drink", DrinkController.create); 
+  app.put("/drink/:id", DrinkController.update); 
+  app.delete("/drink/:id", DrinkController.delete); 
 };
